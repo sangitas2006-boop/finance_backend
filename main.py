6c6337037d73266707b7423303c605f43207ec0a
Aniket <sangitas2006@gmail.com>
2026-04-05 23:12:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI
 import models
 from database import engine
 from routes import transactions
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI(
     title="Finance Backend API",
